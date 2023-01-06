6eaa79709142c505cc3da8271600f3c5bd5bb2e6
cconceicao <conceicaoclaudia5@gmail.com>
2023-01-06 10:24:29 +0000

    Update and rename children-component.jsx to use-case-children-as-component.jsx

--- use-case-children-as-component.jsx
+++ use-case-children-as-component.jsx
@@ -2,7 +2,7 @@
 React components re-render themselves and all their children when the state is updated.
 On every mouse click the state of ClickableComponent is updated, its re-render is triggered, and so, ChildComponent will re-render as well.
 */
-import { useState } from "react";
+import React, { useState } from "react";
 
 export const ChildComponent = () => {
   console.log("Children re-render.");
