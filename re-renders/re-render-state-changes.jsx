8d11251894c2415bfc662013ab386ce6527baf10
claudia.conceicao <claudia.conceicao@farfetch.com>
2023-01-13 11:11:50 +0000

    Update re-render-state-changes.jsx

--- re-renders/re-render-state-changes.jsx
+++ re-renders/re-render-state-changes.jsx
@@ -4,21 +4,13 @@ Component’s state changes -> re-render itself
 
 import React, { useState } from "react";
 
-const Child = () => {
-  console.log("Child re-renders");
-  return <></>;
-};
-
 const App = () => {
   const [state, setState] = useState(1);
   const onClick = () => setState(state + 1);
 
-  return (
-    <>
-      <button onClick={onClick}>click here</button>
-      <Child />
-    </>
-  );
+  console.log("Re-render number: ", state);
+
+  return <button onClick={onClick}>click here</button>;
 };
 
 export default App;
